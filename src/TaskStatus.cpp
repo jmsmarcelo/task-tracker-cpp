1d625366085217e1f6c7dacaf56249d3016df109
jmsmarcelo <jmsmarcelos@gmail.com>
2024-12-29 22:44:37 -0400

    feat: add status_is_valid() function to validate task status

--- src/TaskStatus.cpp
+++ src/TaskStatus.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <stdexcept>
 #include "TaskStatus.hpp"
 
 std::string task::status_to_string(const task::Status& status) {
@@ -23,4 +24,12 @@ task::Status task::status_from_string(const std::string& status) {
         return task::Status::DONE;
     }
     throw std::invalid_argument("invalid task status");
+}
+bool task::status_is_valid(const std::string& status) {
+    try {
+        task::status_from_string(status);
+        return true;
+    } catch(const std::invalid_argument& e) {
+        return false;
+    }
 }
--- src/TaskStatus.hpp
+++ src/TaskStatus.hpp
@@ -9,6 +9,7 @@ namespace task {
     };
     std::string status_to_string(const Status&);
     Status status_from_string(const std::string&);
+    bool status_is_valid(const std::string&);
 }
 
 #endif
